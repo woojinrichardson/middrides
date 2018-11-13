10f83d317e76703c6a8947396a9a9001471efa18
Woojin Richardson <woojin.richardson@gmail.com>
2018-11-12 19:25:32 -0500

    Split request queue into pending requests and in progress requests, and add button to each in progress request to set its state to 'satisfied'

--- src/components/RequestQueue.js
+++ src/components/RequestQueue.js
@@ -61,29 +61,64 @@ class RequestQueue extends Component {
     event.preventDefault();
   }
 
+  makeSatisfied = (event, id) => {
+    const db = firebase.firestore();
+    db.collection('requests').doc(id).update({
+      state: 'satisfied'
+    })
+    .then(() => {
+      const updatedRequests = this.state.requests.filter(request => request.id !== id);
+      this.setState({ requests: updatedRequests })
+    });
+    event.preventDefault();
+  }
+
   render() {
     if (this.state.requests) {
       const pendingRequests = this.state.requests.filter(request => request.state === 'pending');
       const inProgressRequests = this.state.requests.filter(request => request.state === 'in progress');
       return (
-        <table>
-          <tbody>
-            {pendingRequests.map(request => (
-              <tr key={request.user}>
-                <td>{request.name}</td>
-                <td>{request.passengers}</td>
-                <td>{request.from}</td>
-                <td>{request.to}</td>
-                <button onClick={event => this.makeInProgress(event, request.id)}>
-                Pick Up
-                </button>
-                <button onClick={event => this.cancelRequest(event, request.id)}>
-                Cancel Request
-                </button>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <div>
+          <table>
+            <caption><b>Pending Requests</b></caption>
+            <tbody>
+              {pendingRequests.map(request => (
+                <tr key={request.user}>
+                  <td>{request.name}</td>
+                  <td>{request.passengers}</td>
+                  <td>{request.from}</td>
+                  <td>{request.to}</td>
+                  <button onClick={event => this.makeInProgress(event, request.id)}>
+                  Pick Up
+                  </button>
+                  <button onClick={event => this.cancelRequest(event, request.id)}>
+                  Cancel Request
+                  </button>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <br/>
+          <table>
+            <caption><b>In Progress Requests</b></caption>
+            <tbody>
+              {inProgressRequests.map(request => (
+                <tr key={request.user}>
+                  <td>{request.name}</td>
+                  <td>{request.passengers}</td>
+                  <td>{request.from}</td>
+                  <td>{request.to}</td>
+                  <button onClick={event => this.makeSatisfied(event, request.id)}>
+                  Drop Off
+                  </button>
+                  <button onClick={event => this.cancelRequest(event, request.id)}>
+                  Cancel Request
+                  </button>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       );
     }
     return null;
