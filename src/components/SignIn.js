52ee84f6d22bb297de45fafeb26c2a3883627e77
Woojin Richardson <woojin.richardson@gmail.com>
2018-12-08 19:42:26 -0500

    Make SignIn a functional component

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,46 +1,10 @@
-import React, { Component } from 'react';
-// import { withRouter } from 'react-router-dom';
+import React from 'react';
 import { auth } from '../firebase';
-import { firebase } from '../firebase/firebase';
-import { Button, Menu } from 'semantic-ui-react';
+import { Menu } from 'semantic-ui-react';
 
-class SignInButton extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: '',
-    }
-  }
-
-  onClick = (event) => {
-    // const {
-    //   history,
-    // } = this.props;
-
-    auth.signIn()
-      .then(result => {
-        const user = result.user;
-        // history.push('/home');
-      })
-      .catch(error => {
-        this.setState({ error })
-      })
-
-    event.preventDefault();
-  }
-
-  render() {
-    const {
-      error,
-    } = this.state;
-
-    return (
-      <Menu.Item name='sign in' onClick={this.onClick}>
-        Sign in
-      </Menu.Item>
-    );
-  }
-}
+const SignInButton = () =>
+  <Menu.Item name='sign in' onClick={auth.signIn}>
+    Sign in
+  </Menu.Item>
 
 export default SignInButton;
-// export default withRouter(SignInPage);
